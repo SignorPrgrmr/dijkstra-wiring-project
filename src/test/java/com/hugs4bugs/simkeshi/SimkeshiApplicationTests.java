3a29d72162131aae8e118486b2b279a94ca57a0e
amin <signorprgrmr@gmail.com>
2021-05-16 16:37:20 +0430

    Initializing reposityr

---
+++ src/test/java/com/hugs4bugs/simkeshi/SimkeshiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugs4bugs.simkeshi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimkeshiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
