736ffdb31fb704513e88dcbcfa73675bdfc6358b
Abtin Shafiei <abtinshafieiuni@gmail.com>
2021-05-23 22:53:46 +0430

    front design and work of clear and draw fab

--- app/src/main/java/com/example/electricproject/CanvasTest.java
+++ app/src/main/java/com/example/electricproject/CanvasTest.java
@@ -3,21 +3,30 @@ package com.example.electricproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
 public class CanvasTest extends AppCompatActivity {
     private RelativeLayout relativeLayout;
     private TextView twTest;
-    private  float x;
-    private  float y;
-    private  int verticesCount;
-    private ArrayList<float []> vLocation;
+    private float x;
+    private float y;
+    private int verticesCount;
+    private ArrayList<float[]> vLocation;
+    private FloatingActionButton fabDraw;
+    private LinearLayout layoutFabs;
+    private LinearLayout layoutMain;
+    private Button btnClear;
 
     @SuppressLint({"ClickableViewAccessibility", "SetTextI18n"})
     @Override
@@ -26,23 +35,42 @@ public class CanvasTest extends AppCompatActivity {
         setContentView(R.layout.activity_canvas_test);
         twTest = findViewById(R.id.tw_test);
         relativeLayout = findViewById(R.id.relative_layout);
+        fabDraw = findViewById(R.id.fab_draw);
+        layoutFabs = findViewById(R.id.layout_fabs);
+        layoutMain = findViewById(R.id.layout_main);
+        btnClear = findViewById(R.id.btn_clear);
+        btnClear.setOnClickListener((v) -> {
+            Intent intent = new Intent(v.getContext(), CanvasTest.class);
+            startActivity(intent);
+        });
+
+
+        layoutMain.setOnClickListener((v -> {
+            layoutFabs.setVisibility(View.INVISIBLE);
+        }));
+
+
+        fabDraw.setOnClickListener((v) -> {
+            layoutFabs.setVisibility(View.VISIBLE);
+        });
         vLocation = new ArrayList<>();
         relativeLayout.setOnTouchListener((v, event) -> {
             x = event.getX();
             y = event.getY();
-            if (event.getAction() == MotionEvent.ACTION_UP){
+            if (event.getAction() == MotionEvent.ACTION_UP) {
                 twTest.setText("x is: " + x + " and y is: " + y);
                 createBtn();
             }
             return true;
         });
+
     }
 
     @SuppressLint("SetTextI18n")
     private void createBtn() {
         relativeLayout = findViewById(R.id.relative_layout);
         Button btn = new Button(this);
-        RelativeLayout.LayoutParams lp= new RelativeLayout.LayoutParams(80,80);
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(80, 80);
         btn.setLayoutParams(lp);
         btn.setX(x);
         btn.setY(y);
