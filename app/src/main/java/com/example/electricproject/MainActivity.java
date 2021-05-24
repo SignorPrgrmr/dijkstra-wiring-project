cad0a88af9dd2659aad43c1150c345ada97ad956
Asus <afrakhosro@gmail.com>
2021-05-24 20:51:38 +0430

    capture image

--- app/src/main/java/com/example/electricproject/MainActivity.java
+++ app/src/main/java/com/example/electricproject/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.electricproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button takePhotoBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        takePhotoBtn = findViewById(R.id.take_photo_btn);
+
+        takePhotoBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(v.getContext(), captureImage.class);
+            startActivity(intent);
+        });
     }
 }
