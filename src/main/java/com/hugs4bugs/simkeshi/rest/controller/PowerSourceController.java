9966def9422065b6acf62c18f79ebfe218b3e6de
fatemehek <esmkfatemeh@gmail.com>
2021-05-25 16:09:49 +0430

    copmletion of PowerSourceController

--- src/main/java/com/hugs4bugs/simkeshi/rest/controller/PowerSourceController.java
+++ src/main/java/com/hugs4bugs/simkeshi/rest/controller/PowerSourceController.java
@@ -1,6 +1,8 @@
 package com.hugs4bugs.simkeshi.rest.controller;
 
-import com.hugs4bugs.simkeshi.entity.GraphNode;
+import com.hugs4bugs.simkeshi.core.GraphNode;
+import com.hugs4bugs.simkeshi.service.interfaces.PowerSourceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class PowerSourceController {
 
+    private final PowerSourceService service;
+    @Autowired
+    public PowerSourceController(PowerSourceService service) {
+        this.service = service;
+    }
+
     @PostMapping("/graph")
     public GraphNode powerSource(@RequestBody GraphNode powerSource) {
-        return null;
+        return service.findTheOptimumSolution(powerSource);
     }
 }
