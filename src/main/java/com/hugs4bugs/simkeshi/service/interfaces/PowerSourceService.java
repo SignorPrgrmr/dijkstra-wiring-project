4a97377ee631204a9793b6a87aa0d72fba666fcf
fatemehek <esmkfatemeh@gmail.com>
2021-05-25 16:00:04 +0430

    create PowerSourceService and PowerSourceServiceImpl

---
+++ src/main/java/com/hugs4bugs/simkeshi/service/interfaces/PowerSourceService.java
@@ -0,0 +1,8 @@
+package com.hugs4bugs.simkeshi.service.interfaces;
+
+import com.hugs4bugs.simkeshi.core.GraphNode;
+
+public interface PowerSourceService {
+
+    void findTheOptimumSolution(GraphNode head);
+}
