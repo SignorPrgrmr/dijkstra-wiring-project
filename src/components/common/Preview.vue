227ea17eec46f480dc019ec2c6814538b189a5db
Bita <bitakhashechian00@gmail.com>
2021-05-26 11:50:57 -0700

    preview frame

--- src/components/common/Preview.vue
+++ src/components/common/Preview.vue
@@ -1,5 +1,13 @@
 <template>
+<div>
 
+  <div class="preview">
+    <div class="inner">
+
+    </div>
+  </div>
+
+</div>
 </template>
 
 <script>
@@ -13,5 +21,25 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+.preview{
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  background-color: rgba(0, 0, 0, 0.2);
+  display: flex;
+  top: 0;
+  left: 0;
+  justify-content: center;
+  align-items: center;
+}
 
+
+.inner{
+  position: fixed;
+  border-radius: 15px;
+  width: 70%;
+  height: 70%;
+  background-color: aliceblue;
+  margin: 0px;
+}
 </style>
